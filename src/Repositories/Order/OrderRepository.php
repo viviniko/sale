9b2c6c01a2b6f2d336055e4a71f393db68cc80dd
viviniko <viviniko.shop@gmail.com>
2018-01-29 10:52:06 +0800

    :trollface:

--- src/Repositories/Order/OrderRepository.php
+++ src/Repositories/Order/OrderRepository.php
@@ -4,5 +4,21 @@ namespace Viviniko\Sale\Repositories\Order;
 
 interface OrderRepository
 {
+    /**
+     * Paginate the given query into a simple paginator.
+     *
+     * @param null $perPage
+     * @param string $searchName
+     * @param null $search
+     * @param null $order
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function paginate($perPage = null, $searchName = 'search', $search = null, $order = null);
+
+    /**
+     * @param mixed $column
+     * @param null $value
+     * @return boolean
+     */
     public function exists($column, $value = null);
 }
